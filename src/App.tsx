c5f05b5b71aa739217f71a2e8153f3a282b70728
basha-github <sharifabee.alur@gmail.com>
2025-11-12 19:55:55 +0530

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Movies from './components/Movies'
 import Student from './components/Student'
+import Demo from './components/Demo'
 
 export default function App() {
   return (
     <div>
 
 
-<Movies />
-<Student />
+<Demo/>
 
 
     </div>
