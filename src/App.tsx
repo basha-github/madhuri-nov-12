1e4ca62f5001428eab5f47ce937161a2882e112e
basha-github <sharifabee.alur@gmail.com>
2025-11-12 19:57:30 +0530

    v3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import Movies from './components/Movies'
-import Student from './components/Student'
+
 import Demo from './components/Demo'
 
 export default function App() {
